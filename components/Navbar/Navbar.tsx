0301f7527fff4b59e56bfa07583158822d92e769
WONHO22 <dnjsgh2122@gmail.com>
2023-10-31 10:00:00 +0900

    [Fix] 메인화면 route 변경 /#home => /

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 const navItems = [
   {
     id: "item1",
-    route: "/#home",
+    route: "/",
     name: "홈",
   },
   {
@@ -94,7 +94,7 @@ export default function Navbar() {
       variants={navbarAnimation}
       transition={{ duration: 0.35, ease: `linear` }}
     >
-      <Link href="/#home" className="navItem">
+      <Link href="/" className="navItem">
         <motion.div
           {...hoverMotion}
           className="flex text-[2rem] font-bold tracking-tighter"
