a8b5a49f9d2d69c78363630eb565dbdba07e50a8
WONHO22 <dnjsgh2122@gmail.com>
2023-11-15 01:06:17 +0900

    [Design] Footer 컴포넌트 디자인 수정

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-screen text-gray-600 font-semibold text-[0.9em]">
-      <div className="flex justify-center mt-5 gap-4 pb-[1vw] px-[5.4vw] xl:px-[8rem]">
+    <footer className="w-screen text-white/90 text-[0.9em] bg-gray-700">
+      <div className="flex justify-center mt-5 gap-4 py-[1vw] px-[5.4vw] xl:px-[8rem]">
         <Link href="/about">회사소개</Link>
-        <span className="border-l border-gray-600 rounded-md"></span>
+        <span className="border-l border-white rounded-md"></span>
         <Link href="/terms-of-use">서비스 이용약관</Link>
       </div>
-      <div className="border-b border-gray-600 w-screen"></div>
+      <div className="border-b border-white/90 w-screen"></div>
       <div className="items-center mt-4 pb-[1vw] px-[5.4vw] xl:px-[8rem] flex flex-col gap-1">
         <span>탑개미자원　|　대표: 고길현</span>
         <span>고양시 일산서구 가좌동 602</span>
