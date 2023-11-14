d900b18d28758d1ab8a626496e64c4903501aaa1
WONHO22 <dnjsgh2122@gmail.com>
2023-11-14 16:44:27 +0900

    [Feat] 이용약관 컴포넌트 레이아웃 구현

--- app/terms-of-use/page.tsx
+++ app/terms-of-use/page.tsx
@@ -1,11 +1,27 @@
 import Loader from "@/components/Loader/Loader";
+import Image from "next/image";
+import background from "@/public/termsofuseBg.jpg";
 
 const About: React.FC = () => {
   return (
-    <main className="flex flex-col">
-      서비스이용약관
-      <Loader body={"서비스이용약관"} />
-    </main>
+    <section className="w-screen h-[55vh] relative flex justify-center items-center">
+      <div className="flex flex-col absolute top-[40%] left-[15%] w-[70%] text-white">
+        <h1 className="text-[1.6rem] xs:text-[3.2rem] font-bold text-center">
+          서비스이용약관
+        </h1>
+
+        <div className="flex flex-col xs:flex-row w-full justify-start text-[1.4rem] xs:text-[1.7rem]"></div>
+      </div>
+
+      <Image
+        src={background}
+        alt="작가 pressfoto"
+        className="absolute w-screen h-full top-0 left-0 z-[-1] pointer-events-none"
+        style={{ filter: "brightness(50%)" }}
+        quality={80}
+      />
+      <Loader body="서비스이용약관" />
+    </section>
   );
 };
 
