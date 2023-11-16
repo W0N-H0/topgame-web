edfed6166fe6a83d0204f954251d53d5a5b62d1c
WONHO22 <dnjsgh2122@gmail.com>
2023-11-16 18:25:47 +0900

    [Fix] 서버 사이드 렌더링 환경에서 document 객체에 접근할 수 없는 이슈 수정(지도 컴포넌트)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Main from "@/components/Main/Main";
 import Business from "@/components/Business/Business";
+import Map from "@/components/Map/Map";
 import Inquiry from "@/components/Inquiry/Inquiry";
 import TopButton from "@/components/TopButton/TopButton";
 import Loader from "@/components/Loader/Loader";
@@ -9,7 +10,7 @@ export default function Home() {
     <div className="flex flex-col">
       <Main />
       <Business />
-
+      <Map />
       <Inquiry />
       <TopButton />
       <Loader body={"default"} />
