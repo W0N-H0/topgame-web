54a5a92472a91d379ff6bd09b2647e68ecd967b1
WONHO22 <dnjsgh2122@gmail.com>
2023-11-16 17:54:42 +0900

    [Feat] InquiryList 컴포넌트 생성

---
+++ components/Inquiry/InquiryList.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+
+const InquiryList: React.FC = () => {
+  return <div>리스트</div>;
+};
+
+export default InquiryList;
