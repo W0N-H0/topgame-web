69f317537a2647208a2ca3d6deba0ff4419ed0b3
WONHO22 <dnjsgh2122@gmail.com>
2023-11-20 01:24:23 +0900

    [Add] 1. 매입품목 내용 추가, 2. 철거 사진 추가

--- components/Business/BusinessItems.tsx
+++ components/Business/BusinessItems.tsx
@@ -13,6 +13,8 @@ interface IBusinessItem {
   src: string;
   alt: string;
   body: string;
+  body2?: string;
+  body3?: string;
 }
 
 const businesstItems: IBusinessItem[] = [
@@ -20,31 +22,27 @@ const businesstItems: IBusinessItem[] = [
     title: "고철",
     src: "/assets/steel.png",
     alt: "고철 logo",
-    body: `각종 고철류 `,
+    body: `각종 고철류(생철, 중량고철, 경량고철, 가공고철 등)`,
   },
   {
     title: "비철",
     src: "/assets/비철2.jpg",
     alt: "비철 Logo",
-    body: `각종 비철류, 동, 구리, 알루미늄, 스테인리스 등`,
+    body: `각종 비철류(동, 구리, 알루미늄, 스테인리스 등)`,
   },
   {
     title: "컴퓨터/전자기기",
     src: "/assets/컴퓨터2.jpg",
     alt: "컴퓨터/전자기기Logo",
-    body: `못쓰는 컴퓨터 본체, 모니터, 전자기기 등`,
+    body: `못쓰는 컴퓨터 본체, 모니터 및 각종 전자기기 등`,
   },
   {
     title: "철거",
-    src: "/assets/steel.png",
+    src: "/assets/철거.jpg",
     alt: "철거 logo",
     body: `- 상업공간 철거: 음식점, 주점, 의류매장, 체인점 등`,
-  },
-  {
-    title: `test`,
-    src: "/assets/steel.png",
-    alt: `test`,
-    body: `test`,
+    body2: "- 업무공간 철거: 사무실, 공장, 사무집기, 인테리어 등",
+    body3: "- 기계 철거: 인쇄 기계, 실외기, 노후된 기계 및 불용설비 등",
   },
 ];
 
@@ -97,8 +95,17 @@ const BusinessItems = ({ onHoverChange }: IEngagementProps): JSX.Element => {
             >
               here
             </a> */}
-            .
           </p>
+          {item.body2 ? (
+            <p className="text-[1em] lg:text-[1.25em] font-medium">
+              {item.body2}
+            </p>
+          ) : null}
+          {item.body3 ? (
+            <p className="text-[1em] lg:text-[1.25em] font-medium">
+              {item.body3}
+            </p>
+          ) : null}
         </li>
       ))}
     </div>
